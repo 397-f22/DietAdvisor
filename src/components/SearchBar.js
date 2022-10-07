7825dbbeca4bdd9b086c1e00137994be585ab88b
Isaias Lipa <isaiaslipa@Isaiass-MacBook-Pro.local>
2022-10-06 21:04:46 -0500

    search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,79 @@
 import { Form, Button } from "react-bootstrap";
 import nutrients from '../data/menu.jsx';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 
 
-const SearchBar = ({setSelection}) => {
-    
+const handleOnSearch = (string, results) => {
+    // onSearch will have as the first callback parameter
+    // the string searched and for the second the results.
+    console.log(string, results)
+  }
+
+  const handleOnHover = (result) => {
+    // the item hovered
+    console.log(result)
+  }
+
+  const handleOnSelect = (item) => {
+    // the item selected
+    console.log(item)
+  }
+
+  const handleOnFocus = () => {
+    console.log('Focused')
+  }
+const items = [
+    {
+        id: 0,
+        name: 'Cobol'
+    },
+    {
+        id: 1,
+        name: 'JavaScript'
+    },
+    {
+        id: 2,
+        name: 'Basic'
+    },
+    {
+        id: 3,
+        name: 'PHP'
+    },
+    {
+        id: 4,
+        name: 'Java'
+    }
+]
+
+const formatResult = (item) => {
     return (
-        <Form.Control type="search" placeholder="Enter food" onChange={(e) => setSelection(e.target.value)} />
+        <>
+            <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span>
+            <span style={{ display: 'block', textAlign: 'left' }}>name: {item.name}</span>
+        </>
+    )
+}
+
+const SearchBar = ({ setSelection }) => {
+    return (
+        <div>
+            <Form.Control type="search" placeholder="Enter food" onChange={(e) => setSelection(e.target.value)} />
+            <Form.Select aria-label="Default select example">
+                <option>Open this select menu</option>
+                <option value="1">Egg</option>
+                <option value="2">Bread</option>
+                <option value="3">Apple</option>
+            </Form.Select>
+            <ReactSearchAutocomplete>
+                items={items}
+                onSearch={handleOnSearch}
+                onHover={handleOnHover}
+                onSelect={handleOnSelect}
+                onFocus={handleOnFocus}
+                autoFocus
+                formatResult={formatResult}
+            </ReactSearchAutocomplete>
+        </div>
     )
 
 };
