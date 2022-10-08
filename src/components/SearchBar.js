ee100b931e9432025dfac2eb466ec1d1f41a55e6
David Zhang <davidzhang2023@u.northwestern.edu>
2022-10-07 20:31:33 -0500

    update import

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
-import { Form, Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import nutrients from '../data/menu.jsx';
 import foods from "../data/food.json";
