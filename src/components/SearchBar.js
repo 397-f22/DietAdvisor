0cf55ea96577aa6046ab6e022a76a3400ab1d874
MCDK-Tech <davidkim2023@u.northwestern.edu>
2022-10-07 19:45:34 -0500

    SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,27 @@
 import { Form, Button } from "react-bootstrap";
 import nutrients from '../data/menu.jsx';
 import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+import {food} from '../data/menu.jsx'
 
+// const handleOnSearch = (string, results) => {
+//     onSearch will have as the first callback parameter
+//     the string searched and for the second the results.
+//     console.log(string, results)
+//   }
 
-const handleOnSearch = (string, results) => {
-    // onSearch will have as the first callback parameter
-    // the string searched and for the second the results.
-    console.log(string, results)
-  }
+//   const handleOnHover = (result) => {
+//     // the item hovered
+//     console.log(result)
+//   }
 
-  const handleOnHover = (result) => {
-    // the item hovered
-    console.log(result)
-  }
+//   const handleOnSelect = (item) => {
+//     // the item selected
+//     console.log(item)
+//   }
 
-  const handleOnSelect = (item) => {
-    // the item selected
-    console.log(item)
-  }
-
-  const handleOnFocus = () => {
-    console.log('Focused')
-  }
+//   const handleOnFocus = () => {
+//     console.log('Focused')
+//   }
 const items = [
     {
         id: 0,
@@ -60,8 +60,8 @@ const SearchBar = ({ setSelection }) => {
             <Form.Control type="search" placeholder="Enter food" onChange={(e) => setSelection(e.target.value)} />
             <Form.Select aria-label="Default select example">
                 <option>Open this select menu</option>
-                <option value="1">Egg</option>
-                <option value="2">Bread</option>
+                <option value="1"></option>
+                <option value="2">Low Fat Vanilla </option>
                 <option value="3">Apple</option>
             </Form.Select>
             <ReactSearchAutocomplete
