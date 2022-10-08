d8ab8d485b015dddea5390a260b3e83e5d95ef8a
David Zhang <davidzhang2023@u.northwestern.edu>
2022-10-07 20:29:58 -0500

    added food tracking functionality

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   });
   return (
     <div className="App">
-      <SearchBar setMacros={setMacros} />
+      <SearchBar macros={macros} setMacros={setMacros} />
       <NutritionInfo nutrients={macros} />
     </div>
   );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,16 +3,30 @@ import { useState } from 'react';
 import nutrients from '../data/menu.jsx';
 import foods from "../data/food.json";
 
-const SearchBar = ({ setMacros }) => {
-    const [food,setFood] = useState(0); 
+const updateMacros = (item, portions, macros) => {
+    const newMacros = {};
+    Object.assign(newMacros, macros);
+
+    Object.entries(macros).forEach( 
+        ([macro, amount]) => {
+            newMacros[macro] = amount + portions * item[macro];
+        }
+    );
+
+    return newMacros;
+};
+
+const SearchBar = ({ macros, setMacros }) => {
+    const [food,setFood] = useState(Object.keys(foods)[0]); 
+    const [portions, setPortions] = useState(0);
 
     return (
         <div>
             <Form.Select onChange ={(e)=> setFood(e.target.value)}>
                 {Object.keys(foods).map((item) => (<option key={item} value={item}>{item}</option>))}
             </Form.Select>
-            <Form.Control type="search" placeholder="Enter portions (number)" onChange={(e) => setMacros(e.target.value)} />
-            <Button variant="primary" onClick={setMacros()}>Add</Button>
+            <Form.Control type="search" placeholder="Enter portions (number)" onChange={(e) => setPortions(e.target.value)} />
+            <Button variant="primary" onClick={() => {setMacros(updateMacros(foods[food], portions, macros))}}>Add</Button>
             {/* <ReactSearchAutocomplete
                 items={items}
                 onSearch={handleOnSearch}
