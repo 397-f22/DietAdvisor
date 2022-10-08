b03d59d3a04aa29ced8d48def501446e4b6cfe8c
Xiaowei Chen <xiaoweichen@Xiaoweis-MacBook-Air.local>
2022-10-07 21:00:38 -0500

    Add slider bar.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NutritionInfo from './components/NutritionInfo';
 import SearchBar from './components/SearchBar';
 import nutrients from './data/menu';
+import Slider from './components/Slider'
 
 function App() {
   const [selection, setSelection] = useState(null);
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <SearchBar setSelection={setSelection} />
       <NutritionInfo nutrients={nutrients} />
+      <Slider />
     </div>
   );
 }
