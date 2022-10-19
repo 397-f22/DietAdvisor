78dcff42a8566a3b6ed3bbdc84f2d8441e0140df
Xiaowei Chen <rinemiya0203@gmail.com>
2022-10-19 16:24:43 -0500

    Add part of user profile

--- src/components/LoginLogout.js
+++ src/components/LoginLogout.js
@@ -1,24 +1,37 @@
 import { signInWithGoogle, signOutWithGoogle } from "../database/firebase";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 const LoginLogout = () => {
     const Authenticate = () => {
         if (localStorage.getItem("userName")) {
             signOutWithGoogle()
-            // window.location.reload(false);
         } else {
             signInWithGoogle()
-            // window.location.reload(false);
         }
     }
 
     return (
-        <div className="authentication">
+        <Container fluid className="profile-container">
+            <Row className="profile-photo-name">
+                <div>
+                    <img className="profile-photo" src={localStorage.getItem("userPhoto")} referrerpolicy="no-referrer" />
+                </div>
+                <div style={{fontWeight: "bolder", fontSize: "23px"}}>
+                    {localStorage.getItem("userName")}
+                </div>
+            </Row>
+            {/* <Row style={{height: "0vh"}}></Row> */}
+            <Row className="profile-nutrient-goal">
+                <Col>Nutrient goal: </Col>
+            </Row>
+            <Row className="profile-item-list">
+                <Col>List of items: </Col>
+            </Row>
+            <Row className="profile-food-timeseries">
+                <Col>Food timeseries: </Col>
+            </Row>
             <Button onClick={Authenticate}>{localStorage.getItem("userName") ? "Logout" : "Login"}</Button>
-            <div>
-                User: {localStorage.getItem("userName")}
-            </div>
-        </div>
+        </Container>
     )
 }
 
